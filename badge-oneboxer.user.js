6a42db301db7663f8415bda60ed1990939186774
The-Quill <seriouslyquill@gmail.com>
2015-08-22 10:15:40 +1000

    Start

---
+++ badge-oneboxer.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Badge Oneboxer
+// @description Converts tag-like badge comments to badges
+// @version 0.1
+// @match *://chat.stackexchange.com/rooms/*
+// @match *://chat.stackoverflow.com/rooms/*
+// @match *://chat.meta.stackexchange.com/rooms/*
+// @author The-Quill
+// @run-at document-end
+// ==/UserScript==
